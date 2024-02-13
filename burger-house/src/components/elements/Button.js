488abf6fde39c042a615b8871e384dbbf582f6da
ProjetLT <lilian.taillandier@proton.me>
2024-02-13 21:54:25 +0100

    debut creation du bouton

---
+++ burger-house/src/components/elements/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Button() {
+    return(
+        <div>
+            <button className="bg-primary px-5 py-3 font-medium uppercase shadow-xl text-white tracking-widest text-sm">Mon bouton</button>
+        </div>
+    )
+}
--- burger-house/src/components/marketing/pageSection/Header.js
+++ burger-house/src/components/marketing/pageSection/Header.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Logo from '../../../medias/svg/Logo-burger-house.svg';
+import Icone from '../../../medias/svg/ico-bag-clickAndCollect.svg';
+import Button from '../../elements/Button';
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between py-10 bg-gray-500">
-      <div className="bg-red-500 h-20 w-full">
+    <div className="flex items-center justify-between py-10">
+      <div className="w-full">
         <img src={Logo} alt="burger-house" className="w-60" />
       </div>
-      <div className="bg-white h-20 w-full">
-        c'est le moment de goûter nos burger !
+      <div className="w-full text-secondary">
+
+        <div className='flex items-center justify-end'>
+          <img src={Icone} alt="" className='w-5 h-5 mr-1'/>
+          <span>Commander votre repas en ligne</span>
+        </div>
+
+        <div>
+          <Button />
+        </div>
+
       </div>
+
     </div>
   );
 }
